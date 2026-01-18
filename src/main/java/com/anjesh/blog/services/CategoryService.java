da695fcdd7ca1866ce754b0e3e17592f4d27e575
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-01-18 22:54:15 +0530

    List category endpoint logic

---
+++ src/main/java/com/anjesh/blog/services/CategoryService.java
@@ -0,0 +1,11 @@
+package com.anjesh.blog.services;
+
+import com.anjesh.blog.domain.entities.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<Category> listCategories();
+
+}
