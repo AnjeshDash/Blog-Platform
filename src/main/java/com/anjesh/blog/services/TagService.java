8df8fda5e409abb793fb852e745f896cb21ae1e5
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-01-20 00:10:50 +0530

    Implemented Create Tags

--- src/main/java/com/anjesh/blog/services/TagService.java
+++ src/main/java/com/anjesh/blog/services/TagService.java
@@ -3,7 +3,9 @@ package com.anjesh.blog.services;
 import com.anjesh.blog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
